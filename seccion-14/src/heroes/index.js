fcdff3217cb01d214226476a50c674e80f3fc606
eduardo.diturbide <eduardo.diturbide@bancogalicia.com.ar>
2023-04-28 11:07:08 -0300

    Finalizada la sección 14

---
+++ seccion-14/src/heroes/index.js
@@ -0,0 +1,6 @@
+export * from './components'
+export * from './data/Heroes'
+export * from './helpers'
+export * from './hooks'
+export * from './pages'
+export * from './routes/HeroesRouters'
---
+++ seccion-14/src/router/AppRouter.jsx
@@ -0,0 +1,14 @@
+import { Route, Routes, Navigate } from 'react-router-dom'
+import { LoginPage } from '../auth'
+import { HeroesRouters } from '../heroes'
+
+export const AppRouter = () => {
+  return (
+    <>
+      <Routes>
+        <Route path="login" element={<LoginPage />} />
+        <Route path="/*" element={<HeroesRouters />}/>
+      </Routes>
+    </>
+  )
+}
